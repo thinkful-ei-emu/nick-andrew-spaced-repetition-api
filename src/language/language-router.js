f6e03ae9982b5279c94863ab6c430e6be7d7e178
Andrew <andrewyin1994@gmail.com>
2019-09-13 13:07:47 -0700

    removed unnecessary information from head route

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -71,7 +71,7 @@ languageRouter
 
       const head = {
         nextWord: headWord.original,
-        translation: headWord.translation,
+        // translation: headWord.translation,
         totalScore: req.language.total_score,
         wordCorrectCount: headWord.correct_count,
         wordIncorrectCount: headWord.incorrect_count
